158cc1fee4a4b9995aabbbd202efcf12018030e6
Fangang <fangang2004@126.com>
2023-12-13 09:47:23 +0800

    1)add the lombok frameworks 2)clean code

--- alibaba-demo-feign/src/main/java/com/demo/feign/Person.java
+++ alibaba-demo-feign/src/main/java/com/demo/feign/Person.java
@@ -3,47 +3,14 @@
  */
 package com.demo.feign;
 
+import lombok.Data;
+
 /**
  * @author fangang
  */
+@Data
 public class Person {
 	private int id;
 	private String name;
 	private String gender;
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * @return the gender
-	 */
-	public String getGender() {
-		return gender;
-	}
-	/**
-	 * @param gender the gender to set
-	 */
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 }
--- alibaba-demo-service/src/main/java/com/demo/service/Person.java
+++ alibaba-demo-service/src/main/java/com/demo/service/Person.java
@@ -3,47 +3,15 @@
  */
 package com.demo.service;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author fangang
  */
+@Data
 public class Person {
 	private int id;
 	private String name;
 	private String gender;
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * @return the gender
-	 */
-	public String getGender() {
-		return gender;
-	}
-	/**
-	 * @param gender the gender to set
-	 */
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 }
